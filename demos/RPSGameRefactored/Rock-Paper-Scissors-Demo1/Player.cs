fc7dadecea37395b566c61b20a05208da6ee8f05
Mark Moore <mark.moore@revature.com>
2021-12-09 15:19:08 -0600

    Advances the RPG_Game with some refactoring adn abstraction.

---
+++ demos/RPSGameRefactored/Rock-Paper-Scissors-Demo1/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors_Demo1
+{
+    internal class Player
+    {
+        public string Fname{ get; set; }
+        public string Lname { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+
+        public Player(string fname, string lname)
+        {
+            this.Fname = fname;
+            this.Lname = lname;
+        }
+    }
+}
---
+++ demos/RPSGameRefactored/Rock-Paper-Scissors-Demo1/Round.cs
@@ -0,0 +1,15 @@
+﻿namespace Rock_Paper_Scissors_Demo1
+{
+    internal class Round
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+
+        public Player Winner { get; set; }
+
+        public Choice P1Choice { get; set; }
+
+        public Choice P2Choice { get; set; }
+
+    }
+}
